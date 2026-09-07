6de1bb87186a9c6b8dfbb6af57fc15afef5784db
Gabodesign <onofriogabrielecinisi95@gmail.com>
2026-09-07 21:08:18 +0200

    esercizio partycle system

---
+++ Assets/Scripts/SnowController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class SnowController : MonoBehaviour
+{
+    ParticleSystem ps;
+    ParticleSystem.MainModule mainModule;
+    ParticleSystem.EmissionModule emissionModule;
+
+    [Header("Modificabili in Play")]
+    public float startSpeed = 0.5f;
+    public float startSize = 0.2f;
+    public float emissionRate = 20f;
+    public float gravityModifier = 0.1f;
+
+    void Awake()
+    {
+        ps = GetComponent<ParticleSystem>();
+        mainModule = ps.main;
+        emissionModule = ps.emission;
+    }
+
+    void Update()
+    {
+        // Modulo Main
+        mainModule.startSpeed = startSpeed;
+        mainModule.startSize = startSize;
+        mainModule.gravityModifier = gravityModifier;
+
+        // Modulo Emission
+        emissionModule.rateOverTime = emissionRate;
+    }
+}
